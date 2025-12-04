d68407899125563f4df8be76f1f350dce4eb9f52
Gustavo Costa Souza <drt65211@MAC432701-M.local>
2025-12-04 10:15:51 -0300

    feat: Component Hearder

---
+++ src/pages/index.tsx
@@ -0,0 +1,24 @@
+import { Header } from "@/components/header";
+import { Button } from "@/components/ui/button";
+import { Geist, Geist_Mono } from "next/font/google";
+import Blog from "./blog";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export default function Home() {
+  return (
+    <div
+      className={`${geistSans.className} ${geistMono.className} font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20`}
+    >
+      <Header />
+    </div>
+  );
+}
